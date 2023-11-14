72f4c27041c9df6f59ed0afc4ae249b15cc7a9aa
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-14 15:08:50 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,11 +5,12 @@ use PhpAmqpLib\Message\AMQPMessage;
 require_once __DIR__ . '/vendor/autoload.php'; // Include the Composer autoloader
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $rabbitmq_host = 'it490mjt'; // Update with your RabbitMQ server host
+    $rabbitmq_host = '10.198.120.101'; // Update with your RabbitMQ server host
     $rabbitmq_port = 5672;
     $rabbitmq_user = 'it490';
     $rabbitmq_password = 'it490';
-    $rabbitmq_queue = 'tst';
+    $rabbitmq_queue_send = 'userLogin_FTOB';
+    $rabbitmq_queue_receive = 'userLogin_BTOF';
 
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -18,21 +19,53 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = "$username,$password";
 
     // Initialize a connection to RabbitMQ
-    $connection = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
-    $channel = $connection->channel();
-    $channel->queue_declare($rabbitmq_queue, false, true, false, false);
+    $connectionSend = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
+    $channelSend = $connectionSend->channel();
+    $channelSend->queue_declare($rabbitmq_queue_send, false, true, false, false);
 
     // Send the data to RabbitMQ in the desired format
     $message = new AMQPMessage($data);
-    $channel->basic_publish($message, '', $rabbitmq_queue);
+    $channelSend->basic_publish($message, '', $rabbitmq_queue_send);
 
     // Close the connection
-    $channel->close();
-    $connection->close();
+    $channelSend->close();
+    $connectionSend->close();
 
-    // Assuming valid login, send data to RabbitMQ in the desired format
-    echo $message;
-    header("Location: /landing.php");
-    exit;
+    // Receiving Data 
+    $connectionReceive = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
+    $channelReceive = $connectionReceive->channel();
+    $channelReceive->queue_declare($rabbitmq_queue_receive, false, true, false, false);
+
+    //Waiting for a response
+    $callback = function ($msg) {
+	    $response = $msg->body;
+	    if ($response === 'Backend acknowledges successful login attempt') {
+		   header("Location:/landing.php");
+		   exit;
+	    } elseif ($response === 'Backend acknowledges failed message login attempt') {
+		    //Invalid Login display a notification
+		    echo "<script>
+			    document.addEventListener('DOMContentLoaded', function() {
+				var errorBox = document.createElement('div');
+				errorBox.className = 'error-box';
+				errorBox.innerHTML = 'Invalid Login. Please try again.';
+				document.body.appendChild(errorBox);
+
+				setTimeout(function() { 
+					document.body.removeChild(errorBox);	
+				}, 5000);  // Remove the box after 5 seconds
+			    });
+			</script>";
+		    exit;
+	   }
+	   $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
+    };
+    $channelReceive->basic_consume($rabbitmq_queue_receive, '', false, true, false, false, $callback);
+    while (count($channelReceive->callbacks)) {
+	    $channelReceive->wait();
+    }
+    $channelReceive->close();
+    $connectionReceive->close();
+
 }
 ?>
