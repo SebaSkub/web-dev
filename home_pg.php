db2f7f4be2b8afa20ec989451ba049fac70310d2
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-28 20:08:29 -0500

    Create home_pg.php

---
+++ home_pg.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <style>
+        /* General styles for the page */
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f2f2;
+        }
+        
+        /* Style for the navigation bar */
+        nav {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 15px 0;
+        }
+
+        nav a {
+            color: #fff;
+            text-decoration: none;
+            margin: 0 20px;
+        }
+
+        /* Centered content */
+        .content {
+            text-align: center;
+            margin-top: 50px;
+        }
+    </style>
+</head>
+<body>
+    <!-- Navigation bar -->
+    <nav>
+        <a href="/login_pg.php">Login</a>
+        <a href="/register_pg.php">Register</a>
+    </nav>
+
+    <!-- Home page content -->
+    <div class="content">
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page content. You can navigate to the Login or Register page using the links above.</p>
+    </div>
+</body>
+</html>
