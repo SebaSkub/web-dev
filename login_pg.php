8fc42a73cada0abe3524c36dac481c2a7c9f1561
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-28 20:01:59 -0500

    Update login_pg.php

--- login_pg.php
+++ login_pg.php
@@ -13,82 +13,55 @@
             display: flex;
             justify-content: center;
             align-items: center;
-            height: 100vh;
+            min-height: 100vh;
         }
 
         .login-container {
+            width: 90%;
             max-width: 400px;
             background: #fff;
-            border-radius: 5px;
-            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            padding: 30px;
             text-align: center;
         }
 
-        .error-box {
-            position: fixed;
-            top: 0;
-            left: 50%;
-            transform: translateX(-50%);
-            background-color: #ff6961;
-            color: #fff;
-            padding: 10px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.3);
-            z-index: 9999;
-        }
-
-        h2 {
+        .login-container h2 {
+            margin-bottom: 20px;
             color: #333;
         }
 
-        form {
-            padding: 20px;
+        .login-container form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
         }
 
-        table {
+        .login-container input[type="text"],
+        .login-container input[type="password"] {
             width: 100%;
-        }
-
-        td {
-            padding: 10px;
-        }
-
-        input[type="text"],
-        input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin: 5px 0;
+            padding: 12px;
+            margin: 10px 0;
             border: 1px solid #ccc;
-            border-radius: 3px;
+            border-radius: 5px;
             background: #f4f4f4;
             color: #333;
+            box-sizing: border-box;
         }
 
-        input[type="submit"] {
+        .login-container input[type="submit"] {
+            width: 50%;
+            padding: 12px;
+            border: none;
+            border-radius: 5px;
             background-color: #2980b9;
             color: #fff;
-            border: none;
-            border-radius: 3px;
-            padding: 10px 15px;
             cursor: pointer;
             transition: background-color 0.3s;
         }
 
-        input[type="submit"]:hover {
+        .login-container input[type="submit"]:hover {
             background-color: #0077aa;
-        }
-
-        a {
-            text-decoration: none;
-            display: block;
-            text-align: center;
-            margin-top: 10px;
-            color: #2980b9;
-        }
-
-        a:hover {
-            text-decoration: underline;
         }
 
         nav {
@@ -107,25 +80,13 @@
     <div class="login-container">
         <h2>Login</h2>
         <form method="POST" action="login.php" id="loginForm">
-            <table>
-                <tr>
-                    <td>Username:</td>
-                    <td><input type='text' id='username' name='username' required></td>
-                </tr>
-                <tr>
-                    <td>Password:</td>
-                    <td><input type='password' id='password' name='password' required></td>
-                </tr>
-            </table>
-            <br>
-            <input type='submit' value="Login"><br><br>
+            <input type='text' id='username' name='username' placeholder="Username" required>
+            <input type='password' id='password' name='password' placeholder="Password" required>
+            <input type='submit' value="Login">
         </form>
-        <br>
+        <nav>
+            <a href="/register_pg.php">New User? Register here!</a>
+        </nav>
     </div>
-
-    <nav>
-        <a href="/register_pg.php">New User?&ensp;Register here!</a>
-    
-    </nav>
 </body>
 </html>
