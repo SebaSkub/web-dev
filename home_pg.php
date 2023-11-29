903d51e683dfc86a72930260028a06f4d5082615
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-11-28 20:16:17 -0500

    Update home_pg.php

--- home_pg.php
+++ home_pg.php
@@ -5,20 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <style>
-        /* General styles for the page */
         body {
             font-family: 'Arial', sans-serif;
+            background: linear-gradient(to right, #2980b9, #6dd5fa);
             margin: 0;
             padding: 0;
-            background-color: #f2f2f2;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            min-height: 100vh;
+            color: #fff;
         }
-        
-        /* Style for the navigation bar */
+
         nav {
             background-color: #333;
-            color: #fff;
-            text-align: center;
+            width: 100%;
             padding: 15px 0;
+            text-align: center;
         }
 
         nav a {
@@ -27,24 +30,45 @@
             margin: 0 20px;
         }
 
-        /* Centered content */
         .content {
+            max-width: 800px;
             text-align: center;
+            padding: 20px;
+        }
+
+        h1 {
             margin-top: 50px;
+        }
+
+        p {
+            line-height: 1.6;
+        }
+
+        .lol-image {
+            max-width: 100%;
+            height: auto;
+            margin-top: 30px;
         }
     </style>
 </head>
 <body>
-    <!-- Navigation bar -->
     <nav>
         <a href="/login_pg.php">Login</a>
         <a href="/register_pg.php">Register</a>
     </nav>
 
-    <!-- Home page content -->
     <div class="content">
-        <h1>Welcome to the Home Page</h1>
-        <p>This is the home page content. You can navigate to the Login or Register page using the links above.</p>
+        <h1>Explore and Compare Your League of Legends Stats</h1>
+        <p>
+            Welcome to our League of Legends community! Discover a platform where you can dive deep into your in-game statistics and compare your performance with top players worldwide.
+        </p>
+        <p>
+            Our website offers an intuitive way to access your match history, analyze your champion performance, and track your progress. Whether you're a seasoned player or just starting your journey in LoL, our tools help you understand and improve your gameplay.
+        </p>
+        <p>
+            Join us to explore your strengths, identify areas for improvement, and compete with the best. Elevate your League of Legends experience with our comprehensive stat analysis and player comparison features.
+        </p>
+        <img src="download.jpeg" alt="League of Legends Image" />
     </div>
 </body>
 </html>
