fa9d44d6083c8a556d2d569a72d2df175ab7d839
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-12-05 19:13:38 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -1,33 +1,4 @@
-<?php
-require_once __DIR__ . '/vendor/autoload.php';
-
-use PhpAmqpLib\Connection\AMQPStreamConnection;
-use PhpAmqpLib\Message\AMQPMessage;
-
-// Your RabbitMQ connection and queue details
-$rabbitmq_host = '10.198.120.107'; // Update with your RabbitMQ server host
-$rabbitmq_port = 5672;
-$rabbitmq_user = 'it490';
-$rabbitmq_password = 'it490';
-$rabbitmq_queue_receive = 'playerStats';
-
-// Establish connection to RabbitMQ
-$connection = new AMQPStreamConnection($rabbitmq_host, $rabbitmq_port, $rabbitmq_user, $rabbitmq_password);
-$channel = $connection->channel();
-
-// Declare queue to receive messages
-$channel->queue_declare($rabbitmq_queue_receive, false, true, false, false);
-
-// Callback function to process received messages
-$callback = function ($msg) {
-    // Extracting and processing received message
-    $received_message = $msg->body;
-    
-    // Split the received message into individual stats
-    $stats = explode(',', $received_message);
-
-    // Display the statistics (Modify as per your HTML structure)
-    echo "<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
     <title>League of Legends Stats</title>
@@ -97,24 +68,12 @@ $callback = function ($msg) {
             </tr>
         </thead>
         <tbody>
-            <tr>";
-
-    foreach ($stats as $stat) {
-        echo "<td>{$stat}</td>"; // Display each stat in a table cell
-    }
-
-    echo "</tr></tbody></table></body></html>";
-};
-
-// Consume messages from the queue
-$channel->basic_consume($rabbitmq_queue_receive, '', false, true, false, false, $callback);
-
-// Keep the connection open and continue processing messages
-while ($channel->is_open()) {
-    $channel->wait();
-}
-
-// Close the channel and connection after processing
-$channel->close();
-$connection->close();
-?>
+            <tr>
+                <?php foreach ($stats as $stat): ?>
+                    <td><?php echo $stat; ?></td>
+                <?php endforeach; ?>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
