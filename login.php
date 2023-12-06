af339fb8b5115010c44155bed848001bdbd78a9e
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-12-05 20:19:10 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ use PhpAmqpLib\Message\AMQPMessage;
 require_once __DIR__ . '/vendor/autoload.php'; // Include the Composer autoloader
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $rabbitmq_host = '10.198.120.107'; // Update with your RabbitMQ server host
+    $rabbitmq_host = '10.198.120.138'; // Update with your RabbitMQ server host
     $rabbitmq_port = 5672;
     $rabbitmq_user = 'it490';
     $rabbitmq_password = 'it490';
