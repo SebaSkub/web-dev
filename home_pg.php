d70dbc9f8e31b39a4eee6bb1e00ab9dd0c25cb52
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-12-07 18:31:06 -0500

    Update home_pg.php

--- home_pg.php
+++ home_pg.php
@@ -5,88 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <style>
-         body {
+        /* Your CSS styles here */
+        body {
             font-family: 'Arial', sans-serif;
-            background: linear-gradient(to right, #2980b9, #6dd5fa);
             margin: 0;
             padding: 0;
             color: #fff;
-        }
-        
-        .login-container {
-            width: 90%;
-            max-width: 400px;
-            background: #fff;
-            border-radius: 8px;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-            padding: 30px;
-            text-align: center;
-        }
-
-        /* Styles for login form */
-        .login-container h2 {
-            margin-bottom: 20px;
-            color: #333;
-        }
-
-        .login-container form {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .login-container input[type="text"],
-        .login-container input[type="password"] {
-            width: 100%;
-            padding: 12px;
-            margin: 10px 0;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            background: #f4f4f4;
-            color: #333;
-            box-sizing: border-box;
-        }
-
-        .login-container input[type="submit"] {
-            width: 50%;
-            padding: 12px;
-            border: none;
-            border-radius: 5px;
-            background-color: #2980b9;
-            color: #fff;
-            cursor: pointer;
-            transition: background-color 0.3s;
-        }
-
-        .login-container input[type="submit"]:hover {
-            background-color: #0077aa;
-        }
-
-        /* Styles for "Register Here" button */
-        .register-btn {
-            width: 50%;
-            padding: 12px;
-            border: none;
-            border-radius: 5px;
-            background-color: #27ae60;
-            color: #fff;
-            cursor: pointer;
-            transition: background-color 0.3s;
-            margin-top: 10px;
-        }
-
-        .register-btn:hover {
-            background-color: #218e53;
-        }
-
-        /* Styles for error messages */
-        .error-message {
-            color: red;
-            margin-top: 10px;
+            background: url('image1.jpeg') no-repeat center center fixed; /* Set the initial background */
+            background-size: cover;
+            transition: opacity 1s ease-in-out;
         }
 
         nav {
-            background-color: #000; /* Changed to black */
+            background-color: #000;
             width: 100%;
             padding: 15px 0;
             text-align: center;
@@ -102,24 +33,19 @@
             border-radius: 5px;
             transition: all 0.3s ease;
             margin: 0 10px;
-            border: 1px solid #fff; /* Changed border color to white */
+            border: 1px solid #fff;
         }
 
         nav a:hover {
-            background-color: #fff; /* Changed hover background color to white */
-            color: #000; /* Changed hover text color to black */
-        }
-
-        nav a.active {
-            background-color: #f1c40f;
-            color: #333;
+            background-color: #fff;
+            color: #000;
         }
 
         .content {
             max-width: 800px;
             text-align: center;
             padding: 20px;
-            margin-top: 60px; /* Adjusted margin-top to create space below the navbar */
+            margin-top: 60px;
         }
 
         h1 {
@@ -159,5 +85,22 @@
         </p>
         <img class="lol-image" src="download.jpeg" alt="League of Legends Image" />
     </div>
+
+    <script>
+        const images = ['image1.jpeg', 'image2.jpeg', 'image3.jpeg', 'image4.jpeg', 'image5.jpeg'];
+        let currentImageIndex = 0;
+        const body = document.body;
+
+        function changeBackground() {
+            body.style.opacity = '0'; // Fade out
+            setTimeout(() => {
+                body.style.backgroundImage = `url(${images[currentImageIndex]})`;
+                currentImageIndex = (currentImageIndex + 1) % images.length;
+                body.style.opacity = '1'; // Fade in
+            }, 1000); // Change image after 1 second (when fully faded out)
+        }
+
+        setInterval(changeBackground, 5000); // Change image every 5 seconds
+    </script>
 </body>
 </html>
