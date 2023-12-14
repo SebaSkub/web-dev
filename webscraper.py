bfb4190e7980753c36b0a281a731f5e50890efdf
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-12-14 16:27:15 -0500

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,34 +1,44 @@
+from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
-import sys
-import json
+
+app = Flask(__name__)
 
 def scrape_player_data(url):
     response = requests.get(url)
-
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         target_table = soup.find('table', {'class': 'wikitable'})
-
         if target_table:
-            rows_to_skip = 5
-            rows = target_table.find_all('tr')[rows_to_skip:]
-
+            rows = target_table.find_all('tr')[5:]  # Skip headers
             player_data_list = []
             for row in rows:
-                player_data = [cell.get_text(strip=True) for cell in row.find_all(['td', 'th'])]
-                player_data_list.append(player_data)
-
-            return json.dumps(player_data_list)
+                player_stats = row.find_all(['td', 'th'])
+                if len(player_stats) >= 18:  # Ensure there are enough stats to extract
+                    player = {
+                        'name': player_stats[0].get_text(strip=True),
+                        'team': player_stats[1].get_text(strip=True),
+                        'games_played': player_stats[2].get_text(strip=True),
+                        'wins': player_stats[3].get_text(strip=True),
+                        'losses': player_stats[4].get_text(strip=True),
+                        'win_rate': player_stats[5].get_text(strip=True),
+                        'kills': player_stats[6].get_text(strip=True),
+                        'deaths': player_stats[7].get_text(strip=True),
+                        'assists': player_stats[8].get_text(strip=True),
+                        'kda': player_stats[9].get_text(strip=True),
+                        'cs': player_stats[10].get_text(strip=True),
+                        'cs_per_min': player_stats[11].get_text(strip=True),
+                        'gold': player_stats[12].get_text(strip=True),
+                        'gold_per_min': player_stats[13].get_text(strip=True),
+                        'damage': player_stats[14].get_text(strip=True),
+                        'damage_per_min': player_stats[15].get_text(strip=True),
+                        'kill_participation': player_stats[16].get_text(strip=True),
+                        'kill_share': player_stats[17].get_text(strip=True),
+                        'gold_share': player_stats[18].get_text(strip=True),
+                        # Add other fields as required
+                    }
+                    player_data_list.append(player)
+            return player_data_list
         else:
-            return "Table not found on the page."
-    else:
-        return f"Failed to retrieve the page. Status code: {response.status_code}"
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        scraped_data = scrape_player_data(url)
-        print(scraped_data)
-    else:
-        print("Please provide a URL as an argument.")
+            return None
+ 
