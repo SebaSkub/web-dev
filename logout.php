147a0a509ec86006e615e1a47c3df15b8e294c33
SebaSkub <123407738+SebaSkub@users.noreply.github.com>
2023-12-16 17:48:45 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,17 @@
 <?php
-session_start(); // Start the session
+#---------------------------------------------
+#                 UserLogout
+#           By: Sebastian Skubisz
+#---------------------------------------------
+session_start(); # Start the session
 
-// Unset all of the session variables
+# Unset all of the session variables
 $_SESSION = array();
 
-// Destroy the session
+# Destroy the session
 session_destroy();
 
-// Redirect to the login page after logging out
+# Redirect to the login page after logging out
 header("Location: /login_pg.php");
 exit;
 ?>
